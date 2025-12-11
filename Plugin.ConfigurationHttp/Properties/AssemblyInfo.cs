038487a82f4b39bb9c12b95fc51e015786e4c5a5
DKorablin2 <danila.korablin@unity3d.com>
2025-12-11 19:18:47 +0300

    Removed AssemblyCopyright attribute from AssemblyInfo file, because it's automatically added from centrally maintained Directory.Build.targets file

--- Plugin.ConfigurationHttp/Properties/AssemblyInfo.cs
+++ Plugin.ConfigurationHttp/Properties/AssemblyInfo.cs
@@ -5,11 +5,9 @@ using System.Runtime.InteropServices;
 [assembly: System.CLSCompliant(true)]
 
 [assembly: AssemblyDescription("Web UI for service based plugins configuration")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2019-2025")]
 /*
 Register SSL Certificate:
 certhash -> thumbprint
 appid -> Assembly:GuidAttribute
 	netsh http add sslcert ipport=192.168.58.46:8180 certhash=8ede92fb1415e7a975ea20ec26f9466fe7875198 appid={d10da6bc-77fd-4ada-8b3f-b850023e59ae}
-
 */
